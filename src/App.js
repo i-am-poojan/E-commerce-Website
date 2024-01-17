a693434c4e76c63d9b0366acda0bc16d6461f9cc
Poojan Patel <p.poojan1762002@gmail.com>
2024-01-17 12:28:24 +0530

    cart Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Home from './pages/Home';
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Home from "./pages/Home";
+import Loginpage from "./pages/Login";
+import SignupPage from "./pages/Signup"
+import CartPage from "./pages/CartPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <Loginpage></Loginpage>,
+  },
+  {
+    path:"/signup",
+    element:<SignupPage></SignupPage>
+  },{
+    path:"/cart",
+    element:<CartPage></CartPage>
+  }
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,6 @@
+import Login from "../features/auth/components/Login";
+function Loginpage() {
+  return <Login></Login>;
+}
+
+export default Loginpage;
